a1b77b849f980c33e020880387a7809793b77cf5
Kevin <iambevan@gmail.com>
2019-08-15 11:33:04 +0100

    made api key an environment variable

--- src/pages/contact/GoogleMapComponent.tsx
+++ src/pages/contact/GoogleMapComponent.tsx
@@ -17,7 +17,7 @@ const MapWithAMarker = withScriptjs(
 function GoogleMapComponent() {
 	return (
 		<MapWithAMarker
-			googleMapURL="https://maps.googleapis.com/maps/api/js?key=API_KEY&v=3.exp&libraries=geometry,drawing,places"
+			googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
 			loadingElement={<div style={{ height: `100%` }} />}
 			containerElement={<div style={{ height: `100%` }} />}
 			mapElement={<div style={{ height: `100%` }} />}
